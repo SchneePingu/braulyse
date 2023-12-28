2db7b3ff70f676b01b43aa04bab3d765366a3408
SchneePingu <>
2023-12-28 12:37:03 +0100

    add color scheme

--- app/src/main/java/com/example/braulyse/ui/theme/Color.kt
+++ app/src/main/java/com/example/braulyse/ui/theme/Color.kt
@@ -2,10 +2,15 @@ package com.example.braulyse.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DarkPurple = Color(0xFF492971)
+val Purple = Color(0xFF6B57C0)
+val LightPurple = Color(0xFFD0BCFF)
+val PurpleGrey = Color(0xFFb39edd)
+val DarkWhite = Color(0xFFfefefe)
+val DarkWhiteShade = Color(0xFFE4E4E4)
+val LightBlack = Color(0xFF333333)
+val LightBlackShade = Color(0xFF474747)
+val DarkPurpleShade = Color(0xFF3A205A)
+val WhiteShade = Color(0xFFCCCCCC)
+val LightPurpleShade = Color(0xFFD9C9FF)
+val BlackShade = Color(0xFF666666)
--- app/src/main/java/com/example/braulyse/ui/theme/Theme.kt
+++ app/src/main/java/com/example/braulyse/ui/theme/Theme.kt
@@ -10,39 +10,42 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+        primaryContainer = LightPurple,
+        primary = PurpleGrey,
+        secondary = LightPurpleShade,
+        onSecondary = BlackShade,
+        onPrimary = Color.Black,
+        background = LightBlack,
+        surface = LightBlackShade,
+        onSurface = LightPurple,
+        surfaceTint = Color.White
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+        primaryContainer = DarkPurple,
+        primary = Purple,
+        secondary = DarkPurpleShade,
+        onSecondary = WhiteShade,
+        onPrimary = Color.White,
+        background = DarkWhite,
+        surface = DarkWhiteShade,
+        onSurface = DarkPurple,
+        surfaceTint = Color.Black
 )
 
 @Composable
 fun BraulyseTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+        darkTheme: Boolean = isSystemInDarkTheme(),
+        // Dynamic color is available on Android 12+
+        dynamicColor: Boolean = false,
+        content: @Composable () -> Unit
 ) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
@@ -63,8 +66,8 @@ fun BraulyseTheme(
     }
 
     MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
+            colorScheme = colorScheme,
+            typography = Typography,
+            content = content
     )
 }
