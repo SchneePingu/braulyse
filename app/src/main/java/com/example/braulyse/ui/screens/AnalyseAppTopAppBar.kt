cf520ed266c011aea0e80873e564b0e068359d10
SchneePingu <>
2023-12-28 22:22:19 +0100

    adapt font sizes

--- app/src/main/java/com/example/braulyse/ui/screens/AnalyseAppTopAppBar.kt
+++ app/src/main/java/com/example/braulyse/ui/screens/AnalyseAppTopAppBar.kt
@@ -7,8 +7,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.integerResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.braulyse.R
 import com.example.braulyse.ui.theme.BraulyseTheme
 
@@ -24,7 +26,7 @@ fun AnalyseAppTopAppBar(modifier: Modifier = Modifier) {
         title = {
                 Text(
                     text = stringResource(R.string.app_name),
-                    style = MaterialTheme.typography.headlineLarge,
+                    fontSize = integerResource(R.integer.app_name_font_size).sp
                 )
         },
         modifier = modifier
