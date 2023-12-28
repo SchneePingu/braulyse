b7b7e8cfca99fdde8459d4ce93648e798de320db
SchneePingu <>
2023-12-28 22:26:30 +0100

    adapt color scheme

--- app/src/main/java/com/example/braulyse/ui/theme/Color.kt
+++ app/src/main/java/com/example/braulyse/ui/theme/Color.kt
@@ -2,8 +2,8 @@ package com.example.braulyse.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val DarkOrange = Color(0xfff9aa53)
-val LightOrange = Color(0xFFFAC386 )
+val DarkOrange = Color(0xfff5b324)
+val LightOrange = Color(0xFFF9D17B)
 val Blue = Color(0xff007ac1)
 val LightBlue = Color(0xffB2D7EC)
 
