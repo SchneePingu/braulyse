577dc764a8295011de91ae9066e80124a8a7f1b3
SchneePingu <>
2023-12-30 16:35:38 +0100

    adapt default density value

--- app/src/main/java/com/example/braulyse/service/DensityMeasurementService.kt
+++ app/src/main/java/com/example/braulyse/service/DensityMeasurementService.kt
@@ -3,7 +3,7 @@ package com.example.braulyse.service
 import com.example.braulyse.model.DensityMeasurement
 
 private const val DEFAULT_INITIAL_DENSITY = 1.050
-private const val DEFAULT_FINAL_DENSITY = 1.010
+private const val DEFAULT_FINAL_DENSITY = 1.021
 
 private const val MIN_RELATIVE_DENSITY = 1.000
 private const val MAX_RELATIVE_DENSITY = 1.083
